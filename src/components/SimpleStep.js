471f0868fe79e82e454fde2e32d26bded9b8cd56
Arkadiusz Matlak <piccolo@poczta.fm>
2023-06-29 18:41:24 +0200

    improvments

--- src/components/SimpleStep.js
+++ src/components/SimpleStep.js
@@ -2,7 +2,7 @@ import styles from "./SimpleStep.module.css";
 function SimpleStep(props) {
     return(
         <div className={styles.container}>
-            <img className={styles.image} src={`../assets/${props.step.image}`} alt={`Ikona ${props.step.title}`}/>
+            <img className={styles.image} src={`/assets/${props.step.image}`} alt={`Ikona ${props.step.title}`}/>
             <p className={styles.textTitle}>{props.step.title}</p>
             <p className={styles.textDescription}>{props.step.description}</p>
         </div>
