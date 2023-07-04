0c0fda99e5580367387febac88ce1448d2046d41
Arkadiusz Matlak <piccolo@poczta.fm>
2023-07-04 12:51:46 +0200

    firebase connection - new user process implemented

--- src/components/NewUserContainer.js
+++ src/components/NewUserContainer.js
@@ -2,11 +2,11 @@ import styles from "./NewUserContainer.module.css";
 import Header from "@/components/Header";
 import Link from "next/link";
 import NewUserForm from "@/components/NewUserForm";
-function NewUserContainer(props) {
+function NewUserContainer() {
     return (
         <div className={styles.container}>
             <Header text={"Załóż konto"} />
-            <NewUserForm setEmail={props.setEmail} setPassword={props.setPassword} handleSubmit={props.handleSubmit} />
+            <NewUserForm  />
             <button className={styles.newUser}>
                 <Link href={"/login"}>Zaloguj się</Link>
             </button>
--- src/components/NewUserForm.js
+++ src/components/NewUserForm.js
@@ -1,16 +1,42 @@
 import styles from "@/components/NewUserForm.module.css";
+import {useState} from "react";
+import {useRouter} from "next/navigation";
+import {createUserWithEmailAndPassword} from "firebase/auth";
+import {auth} from "@/lib/db";
 
-function NewUserForm(props) {
+function NewUserForm() {
+
+
+    const [error, setError] = useState(false)
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const router = useRouter();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        createUserWithEmailAndPassword (auth, email, password)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                console.log(user);
+                router.push('/');
+            })
+            .catch((error) => {
+                setError(true)
+            });
+    }
     return (
-        <form onSubmit={props.handleSubmit} className={styles.container}>
+        <form onSubmit={handleSubmit} className={styles.container}>
             <div className={styles.inputsContainer}>
                 <div className={styles.inputControl}>
                     <label>Email</label>
-                    <input type="email" onChange={e=> props.setEmail(e.target.value)}/>
+                    <input type="email" onChange={e=> setEmail(e.target.value)}/>
                 </div>
                 <div className={styles.inputControl}>
                     <label>Hasło</label>
-                    <input type="password" onChange={e=> props.setPassword(e.target.value)} />
+                    <input type="password" onChange={e=> setPassword(e.target.value)} />
                 </div>
                 <div className={styles.inputControl}>
                     <label>Powtórz hasło</label>
